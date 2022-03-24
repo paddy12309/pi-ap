9afaca744091bcb93c1e02152332213ed9cbc826
Patrick West <paddy12309@icloud.com>
2022-03-24 17:06:02 +0000

    more more more

--- firewall_ipv4.sh
+++ firewall_ipv4.sh
@@ -106,9 +106,9 @@ cat <<EOF> /etc/ufw/user.rules
 ### tuple ### allow tcp 443 0.0.0.0/0 any 192.168.0.0/28 in
 -A ufw-user-input -p tcp --dport 443 -s 192.168.0.0/28 -j ACCEPT
 
-### tuple ### allow tcp 1883 0.0.0.0/0 any 192.168.5.0/28 in
--A ufw-user-input -p tcp --dport 1883 -j ACCEPT
-#-A ufw-user-output -p tcp --dport 1883 -s 192.168.5.0/28 -j ACCEPT
+### tuple ### allow tcp 1883 0.0.0.0/0 any 192.168.0.0/28 in
+-A ufw-user-input -p tcp --dport 1883 -s 192.168.0.0/28 -j ACCEPT
+#-A ufw-user-output -p tcp --dport 1883 -s 192.168.0.0/28 -j ACCEPT
 #-A ufw-user-allow -p tcp --dport 1883 -s 192.168.0.0/28 -j ACCEPT
 
 ### tuple ### allow udp 1883 0.0.0.0/0 any 192.168.5.0/28 in
