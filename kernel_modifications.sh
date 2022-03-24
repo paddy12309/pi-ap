72ead0d01e2e7d97ff58e0c3a92013795cbd6c37
Patrick West <paddy12309@icloud.com>
2022-03-24 12:11:53 +0000

    Minor remove of ipv4 forward

--- kernel_modifications.sh
+++ kernel_modifications.sh
@@ -23,7 +23,8 @@ source "${BASH_SOURCE%/*}/functions.sh"
 ### sysctl changes:
 
 # Enable Forwarding:
-sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
+# removing ipv4 forwarding
+#sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 sed -i "s/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/" /etc/sysctl.conf
 
 # Read the new changes:
